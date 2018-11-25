94293d4540fdfecd79444d818dc70bbdb28f465b
Anson Gurung <gurunga4@cardiff.ac.uk>
2018-11-25 18:18:33 +0000

    Added different templates for question types, Next/Back functionality

--- BACKonLINEServer.py
+++ BACKonLINEServer.py
@@ -5,17 +5,53 @@ import sqlite3
 DATABASE = "BACKonLINE.db"
 conn = sqlite3.connect(DATABASE)
 cur = conn.cursor()
-
+questionID = 1
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
-@app.route("/getquestion")
+@app.route("/getquestion", methods =["GET", "POST"])
 def getquestion():
+	global questionID
+	options_list, question, questionNum, template = return_question()
+
+
+
+	# resp.set_cookie('questionID', '1')
+	if request.method == "POST":
+		if request.form["submit_button"] == "Next":
+			hey = request.args.get("question-options")
+			print(hey)
+			test1 = request.form["option"]
+			print("TES",test1)
+			hey1 = request.args.get("option")
+			print("es",hey1)
+			question_result = request.form.getlist("option")
+			for result in question_result:
+				print(f"{result} has a value of {return_from_db(result)[0]}")
+			questionID = questionID + 1
+			options_list, question, questionNum, template = return_question()
+			print("increment")
+			#resp.set_cookie('questionID', str(questionID))
+			#return resp
+		elif request.form["submit_button"] == "Back":
+			print("back pressed")
+			questionID = questionID - 1
+			options_list, question, questionNum, template = return_question()
+			print("decrement")
+			#resp.set_cookie('questionID', str(questionID))
+			#return resp
+	resp = make_response(render_template(template, name = "Humzah", question = question[0], options = options_list, questionNum = questionNum))
+	return resp
+def return_question():
+	global questionID
+	#questionID = int(request.cookies.get('questionID'))
+	print("testQ",questionID)
+
 	conn = sqlite3.connect(DATABASE)
 	cur = conn.cursor()
-	questionID = 1
+
 	cur.execute("SELECT questionID FROM Questions WHERE questionID = ?", (questionID,))
 	questionNum = cur.fetchone()
 	cur.execute("SELECT question FROM Questions WHERE questionID = ?", (questionID,))
@@ -23,13 +59,37 @@ def getquestion():
 	cur.execute("SELECT optionText FROM Options WHERE questionID = ?", (questionID,))
 	options = cur.fetchall()
 	print(question,options,questionNum)
+	cur.execute("SELECT questionType FROM Questions WHERE questionID =?",(questionID,))
+	# type = cur.fetchone()
+	cur.execute("SELECT questionType FROM QuestionTypes WHERE typeID =?",(cur.fetchone()))
+	question_type = cur.fetchone()[0]
+	if question_type == "Multiple-Answer":
+		print("MULT")
+		template = "Multiple-Answer.html"
+	elif question_type == "Single-Answer":
+		print("SINGLE")
+		template = "Single-Answer.html"
+	elif question_type == "Slider":
+		print("Slider")
+		template = "Slider.html"
+	elif question_type == "Text-Field":
+		print("TEXT FIELD")
+		template = "Text-Field.html"
+	else:
+		print("There is something wrong",question_type)
 
-	resp = make_response(render_template('Template1.html', name = "Humzah", question = question[0], options = options, questionNum = questionNum))
-	resp.set_cookie('questionID', '1')
+	options_list = []
 
+	for option in options:
+		options_list.append(option[0])
+	return options_list, question, questionNum, template
 
-	return resp
-
+def return_from_db(item):
+	conn = sqlite3.connect(DATABASE)
+	cur = conn.cursor()
+	cur.execute("SELECT questionValue FROM Options Where optionText = ?",(item,))
+	# print(cur.fetchone())
+	return cur.fetchone()
 
 # Hard codeed - testing if server is successfully processes data into the DB
 # @app.route("/getquestion")
