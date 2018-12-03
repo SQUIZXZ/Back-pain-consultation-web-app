6e7423af8231a154811db77dcf5f6668a3381a98
Anson Gurung <gurunga4@cardiff.ac.uk>
2018-12-03 14:33:05 +0000

    Fixed inserting the results into database so that they are not just hard-coded values and updated the return from databse function

--- BACKonLINEServer.py
+++ BACKonLINEServer.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect, request, render_template, make_response, escape, session
+from flask import Flask, redirect, request, render_template, make_response, escape, session, url_for
 import sqlite3
 
 DATABASE = "BACKonLINE.db"
@@ -10,18 +10,26 @@ app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-
+@app.route("/", methods = ["GET","POST"])
+def show_home():
+	return redirect(url_for('static', filename='Login.html'))
+@app.route("/login", methods = ["GET","POST"])
+def login():
+	if request.method == "POST":
+		print("acualy working")
+		username = request.form["username"]
+		password = request.form["password"]
 @app.route("/getquestion", methods =["GET", "POST"])
 
 def getquestion():
 	global questionID
 	options_list, question, questionNum, template = return_question()
-
+	attemptNumber = 1
 
 
 	# resp.set_cookie('questionID', '1')
 	if request.method == "POST":
-		if request.form["submit_button"] == "Next >":
+		if "Next" in request.form:
 			hey = request.args.get("question-options")
 			#print(hey)
 			test1 = request.form["option"]
@@ -30,21 +38,21 @@ def getquestion():
 			#print("es",hey1)
 			question_result = request.form.getlist("option")
 
+			# for result in question_result:
+			# 	if result == "":
+			# 		pass
+			# 	else:
+			# 		print(f"{result} has a value of {return_from_db(result,questionID)[0]}")
+			# 	#print(f"{result} has a value of {return_from_db(result)[0]}")
 
-			for result in question_result:
-				if result == "":
-					pass
-				else:
-					print(f"{result} has a value of {return_from_db(result)[0]}")
-				#print(f"{result} has a value of {return_from_db(result)[0]}")
-			writethings( question_result,questionNum)
+			writethings( question_result,questionNum,attemptNumber)
 			questionID = questionID + 1
 			options_list, question, questionNum, template = return_question()
 			#print("increment")
 
 			#resp.set_cookie('questionID', str(questionID))
 			#return resp
-		elif request.form["submit_button"] == "< Back":
+		elif "Back" in request.form:
 			#print("back pressed")
 			questionID = questionID - 1
 			options_list, question, questionNum, template = return_question()
@@ -53,26 +61,50 @@ def getquestion():
 			#return resp
 	resp = make_response(render_template(template, name = "Humzah", question = question[0], options = options_list, questionNum = questionNum))
 	return resp
-def writethings(question_result,question_number):
+def writethings(question_result,question_number,attemptNumber):
 	print("Values passed", question_result, question_number)
 	print("Working")
 	values = []
+	option_ids = []
 	for result in question_result:
 
-		print("Inside",result)
-		value = return_from_db(result)[0]
-		print("restunred value",value)
-		values.append(value)
-		try:
-			conn.connect(DATABASE)
-			cur = conn.cursor()
-			cur.execute("SELECT optionID FROM Options WHERE optionText = ?", (result,))
-			fetched = cur.fetchone()
-			print("Fetched ID for ***", result)
-		except:
-			pass
-	print("values list",values)
+		print("Inside",result, str(result))
+		value_of_result, id_of_result = return_from_db(result,question_number)
+		print("restunred value",value_of_result)
+		print("ID RETURNED", id_of_result)
+		values.append(value_of_result[0])
+		option_ids.append(id_of_result[0])
+		print("TRYING..")
+		# conn = sqlite3.connect(DATABASE)
+		# cur = conn.cursor()
+		# print("TRYING....")
+# 		try:
+# #### Need to change the select so it selects the optino fromt he correct question
+#
+# 			# cur.execute("SELECT optionID FROM Options INNER JOIN Questions ON Options.questionID = ? and Questions.questionID = ? AND Options.optionText = ?",(question_number,question_number,result,))
+# 			cur.execute("SELECT optionID FROM Options WHERE optionText = ? AND questionID = ?", (str(result),question_number[0],))
+# 			fetched = cur.fetchone()
+# 			if fetched == None:
+# 				cur.execute("SELECT optionID FROM Options WHERE questionID = ?", (question_number[0],))
+# 				fetched = cur.fetchone()
+#
+# 			print("Fetched ID for ***", result, fetched)
+# 		except:
+# 			print("SOMETHING WENT WRONG")
+	if len(values) == 1:
+		values = values[0]
+	else:
+		str_values = [str(value) for value in values]
+		values = ",".join(str_values)
+	if len(option_ids) == 1:
+		option_ids = option_ids[0]
+	else:
+		str_ids = [str(id) for id in option_ids]
+		option_ids = ",".join(str_ids)
 
+	print("values list",values)
+	print("ids list", option_ids)
+	text = ""
 	#result_value = return_from_db(q_result)[0]
 	#need to get the quetsionID and the optionID for each question added
 	try:
@@ -81,7 +113,12 @@ def writethings(question_result,question_number):
 		print('into the try111')
 		cur = conn.cursor()
 		print('into the try222')
-		cur.execute("INSERT INTO Results ('patientID', 'questionID', 'optionID', 'optionValue') VALUES (?,?,?,?)",(1,question_number[0],2,values))
+
+		if question_number[0] == 3:
+			text = question_result[0]
+			print("NEW TEXT",text)
+		print(f"before insert Q:{question_number[0]} Option ID: {option_ids} Text: {text} Option Values:{values} Attempt: {attemptNumber} ")
+		cur.execute("INSERT INTO Results ('patientID', 'questionID', 'optionID', 'optionValue', 'textField','attemptNumber') VALUES (?,?,?,?,?,?)",(1,question_number[0],option_ids,values,text,attemptNumber))
 		print('into the try333')
 		conn.commit()
 		conn.close()
@@ -95,8 +132,6 @@ def writethings(question_result,question_number):
 
 		print('finllay')
 		return msg
-
-
 	# print("Writing data into database")
 	# #print('tititititi')
 	# # if request.method == "POST":
@@ -141,6 +176,7 @@ def return_question():
 	question = cur.fetchone()
 	cur.execute("SELECT optionText FROM Options WHERE questionID = ?", (questionID,))
 	options = cur.fetchall()
+
 	#print(question,options,questionNum)
 	cur.execute("SELECT questionType FROM Questions WHERE questionID =?",(questionID,))
 	# type = cur.fetchone()
@@ -164,20 +200,28 @@ def return_question():
 	options_list = []
 	for option in options:
 		options_list.append(option[0])
+	print("LIST OF OPTIONS FOR QUESTION",options_list)
 	return options_list, question, questionNum, template
 
-def return_from_db(item):
-	#print("Returning values",item)
+def return_from_db(item, question_number):
+	print("Returning values",item, question_number)
 	conn = sqlite3.connect(DATABASE)
 	cur = conn.cursor()
 	cur.execute("SELECT questionValue FROM Options Where optionText = ?",(item,))
-	fetched = cur.fetchone()
+	fetched_value = cur.fetchone()
 	#print("fetched",fetched)
-	if fetched == None: #if the question has a text field, then will return a NoneType
-		fetched = (0,)
+	if fetched_value == None: #if the question has a text field, then will return a NoneType
+		fetched_value = (0,)
+	cur.execute("SELECT optionID FROM Options WHERE optionText = ? AND questionID = ?", (str(item),question_number[0],))
+	fetched_id = cur.fetchone()
+	if fetched_id == None:
+		cur.execute("SELECT optionID FROM Options WHERE questionID = ?", (question_number[0],))
+		fetched_id = cur.fetchone()
+	print("Fetched Value ******", fetched_value)
+	print("Fetched ID for ***", item, fetched_id)
 	# print(cur.fetchone())
 	cur.close()
-	return fetched
+	return fetched_value, fetched_id
 
 # Hard codeed - testing if server is successfully processes data into the DB
 # @app.route("/getquestion")
